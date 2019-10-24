51fc39d462d886b99aa671a38bbc4a8f48649546
Diogo Almeida <diogo254@gmail.com>
2019-10-24 17:34:08 -0300

    Commit da classe Usuario
    Criado propriedades e métodos getters/setters

---
+++ src/TccMainClasse.java
@@ -0,0 +1,10 @@
+
+
+public class TccMainClasse {
+    public static void main(String[] args) {
+        Usuario user = new Usuario(1, "Diogo", "diogo254@gmail.com", "8k2eThWS", 0);
+
+        System.out.println(user.getNivelAcesso());
+
+    }
+}
